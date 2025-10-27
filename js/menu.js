b028eacd81a6e49a6f584832bc5d7c1847051878
jaozintheboy <jpaseixas10@gmail.com>
2025-10-27 07:57:43 -0300

    versão final

--- js/menu.js
+++ js/menu.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.getElementById("menu-btn");
   const menu = document.getElementById("menu");
   if (menuBtn && menu) menuBtn.addEventListener("click", () => menu.classList.toggle("open"));
+document.getElementById("contrasteBtn").addEventListener("click", () => {
+  document.body.classList.toggle("alto-contraste");
+});
 });
